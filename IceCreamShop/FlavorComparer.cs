ecdb4da912c07e4b9510940b2335a16d03da811d
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-10-26 13:26:34 -0400

    modified flavorComparer to compare based on the number of allergens

--- IceCreamShop/FlavorComparer.cs
+++ IceCreamShop/FlavorComparer.cs
@@ -9,9 +9,18 @@ namespace IceCreamShop
         {
         }
 
+        //original compare based on name
+        //public int Compare(Flavor x, Flavor y)
+        //{
+        //    return string.Compare(x.Name, y.Name);
+        //}
+
+        //new compare based on flavor allergens
         public int Compare(Flavor x, Flavor y)
         {
-            return string.Compare(x.Name, y.Name);
+            if (x.Allergens.Count > y.Allergens.Count) return 1;
+            if (x.Allergens.Count == y.Allergens.Count) return 0;
+            return -1;
         }
     }
 }
